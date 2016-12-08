f769a3d7ddb413eaf4a5b68d2752b00c64581f09
Régis ROY <regis.roy@generativeobjects.com>
2016-12-08 12:16:44 +0100

    ajout des tests sur l'utilisation de ressources
    - Fichier
    - Propriétés

---
+++ UsingResources/UsingResourcesTestCases.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+
+namespace CSharp.Resources
+{
+    [TestClass]
+    public class UsingResourcesTestCases
+    {
+        [TestMethod]
+        public void GettingFileResources()
+        {
+            var databaseCreationFileContent = UsingResources.Properties.Resources.database_creation;
+            Check.That(databaseCreationFileContent).Equals("DROP DATABASE Cool\r\n" +
+                                                           "GO\r\n" +
+                                                           "CREATE DATABASE Cool\r\n" +
+                                                           "GO");
+        }
+
+        [TestMethod]
+        public void GettingProperties()
+        {
+            var projectName = UsingResources.Properties.Resources.ProjectName;
+            Check.That(projectName).Equals("Nom du projet dans une ressource de type string");
+        }
+    }
+}
