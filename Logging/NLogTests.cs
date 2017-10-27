4426d471c82879853e148f565a415bfee741b953
Gitlab Runner <gitlab-runner@generativeobjects.com>
2017-10-27 17:03:11 +0200

    add logging project with NLog

---
+++ Logging/NLogTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NLog;
+using NUnit.Framework;
+
+namespace Logging
+{
+    [TestFixture]
+    public class Tests
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        [Test]
+        public void NLog_Tests()
+        {
+            
+            var exception = new Exception("Ce est une exception");
+            var exception2 = new Exception("Ce est une exception", exception);
+            logger.Error(exception2);
+        }
+    }
+}
