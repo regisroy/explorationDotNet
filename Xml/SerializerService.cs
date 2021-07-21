2a6bbb03d6d1e4f820633ae2825bbd10656aee28
Régis ROY <regis.roy@laposte.fr>
2021-07-21 09:04:26 +0200

    refactor using keyword

--- Xml/SerializerService.cs
+++ Xml/SerializerService.cs
@@ -9,20 +9,16 @@ namespace Xml
         {
             var ser = new XmlSerializer(typeof(T));
 
-            using (var sr = new StreamReader(filepath))
-            {
-                return (T) ser.Deserialize(sr);
-            }
+            using var sr = new StreamReader(filepath);
+            return (T) ser.Deserialize(sr);
         }
 
         public static void SerializeToXml<T>(T anyobject, string xmlFilePath)
         {
             var xmlSerializer = new XmlSerializer(anyobject.GetType());
 
-            using (var writer = new StreamWriter(xmlFilePath))
-            {
-                xmlSerializer.Serialize(writer, anyobject);
-            }
+            using var writer = new StreamWriter(xmlFilePath);
+            xmlSerializer.Serialize(writer, anyobject);
         }
     }
 }
